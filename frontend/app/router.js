55aa0e55c70454f74319099f74bd856e3aea1b7f
electreason <electeason@yahoo.fr>
2016-09-09 09:40:25 +0200

    some changes

--- frontend/app/router.js
+++ frontend/app/router.js
@@ -6,31 +6,12 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  // this.resource('features', function() {
-  //   this.route('new');
-  //   this.resource('feature', {path: ':feature_id'}, function(){
-  //     this.route('show');
-  //     this.route('edit');
 
-  //   });
-  // });
-
-
-
-  // this.route('features', function(){
-  //   this.route('new');
-  //   this.route('edit', {path: ':feature_id'});
-
-
-  // });
-
-  // this.route('features-edit', {path: '/features/:feature_id'});
 
 
 this.route('features', function() {
     this.route('new');
-    this.route('feature', { path: ':id' }
-    );
+    this.route('feature', { path: ':id' });
   });
 
 
--- frontend/app/routes/features/feature.js
+++ frontend/app/routes/features/feature.js
@@ -2,38 +2,14 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 
-  model(params) {
-    var feature = this.get('store').findRecord('feature', params.id);
-    alert(feature);
+  model() {
+
+    alert("something");
 
   },
 
 
   actions: {
-    createSong() {
-      let controller = this.get('controller'),
-          band = controller.get('model');
 
-      let song = this.store.createRecord('song', {
-        title: controller.get('newTitle'),
-        band: band
-      });
-      song.save().then(function() {
-        controller.set('newTitle', '');
-      })
-      ['catch'](function(error) {
-        console.error(error);
-      });
-    },
-
-    setRating(params) {
-      let { item: song, rating } = params;
-
-      if (song.get('rating') === rating) {
-        rating = null;
-      }
-      song.set('rating', rating);
-      return song.save();
-    }
   }
 });
